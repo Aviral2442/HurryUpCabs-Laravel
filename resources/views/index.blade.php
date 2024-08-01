5af7a4b004016582590cf7644322ec42b74cc699
Aviral2442 <aviralsrivastava24y15@gmail.com>
2024-08-01 17:15:46 +0530

    bootstrap setup

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,11 @@
 @extends('layout.mainlayout')
 @section('main')
     
-    <h1>Hurry Up</h1>
+    {{-- <h1>Hurry Up</h1>
     <button type="button" class="btn btn-primary">Primary</button>
-    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button> --}}
 
     {{-- services --}}
-    @include('sections.service')
+    {{-- @include('sections.service') --}}
 
 @endsection
